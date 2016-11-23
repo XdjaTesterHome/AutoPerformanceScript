6abff73007baa011f796d7d8edff25dac6beaf07
lzz <lzz@xdja.com>
2016-11-23 17:08:08 +0800

    t\提交代码

--- controller/GetCpuDataThread.py
+++ controller/GetCpuDataThread.py
@@ -3,6 +3,8 @@
 __author__ = 'zhouliwei'
 
 import  threading
+import util.AndroidUtil
+import util.AdbUtil
 """
 function: 用于采集cpu数据的逻辑
 date:2016/11/23
@@ -10,6 +12,13 @@ date:2016/11/23
 """
 
 class GetCpuDataThread(threading.Thread):
+    times=30 #收集数据条数为30次
+#     cycletime=60*60*4 #测试持续时长为4H
+    cycletime=5*60 #测试持续时长为5min
+    interval=cycletime/times#收集1次所需要的时间，单位为s
+    CPUdata=[]#用于收集所有的CPU数据
+    CPUerror=[]#用于手机CPU占用过高的数据。
+ 
     def __init__(self, thread_id):
         threading.Thread.__init__(self)
         self.threadId = thread_id
@@ -18,4 +27,19 @@ class GetCpuDataThread(threading.Thread):
         获取cpu数据的逻辑
     """
     def run(self):
+        while i<times:
+            cpudata=AndroidUtil.get_cpu_data()#当前采集到的数据
+            if cpudata>=50.00:
+                cpuerror=cpudata
+                CPUerror.append(cpuerror)
+                AdbUtil.screenshot()
+            else:
+                pass
+            CPUdata.append(cpudata)
+            i+=1
+        return CPUerror,CPUdata
         pass
+    
+    
+    
+    
--- controller/GetMemoryThread.py
+++ controller/GetMemoryThread.py
@@ -3,6 +3,8 @@
 __author__ = 'lzz'
 
 import  threading
+import util.AndroidUtil
+import util.AdbUtil
 """
 function: 采集内存数据的逻辑
 date:2016/11/23
@@ -11,6 +13,14 @@ date:2016/11/23
 
 
 class GetMemoryDataThread(threading.Thread):
+    
+    times=30 #收集数据条数为30次
+#     cycletime=60*60*4 #测试持续时长为4H
+    cycletime=5*60 #测试持续时长为5min
+    interval=cycletime/times#收集1次所需要的时间，单位为s
+    Memorydata=[]#用于收集所有的内存数据
+    Memoryerror=[]#用于手机内存占用过高的数据。
+    
     def __init__(self, thread_id):
         threading.Thread.__init__(self)
         self.threadId = thread_id
@@ -19,4 +29,15 @@ class GetMemoryDataThread(threading.Thread):
         采集内存数据的逻辑
     """
     def run(self):
+        while i<times:
+            memorydata=AndroidUtil.get_memory_data()#当前采集到的数据
+            if memorydata>=50*1024:
+                memoryerror=memorydata
+                Memoryerror.append(memoryerror)
+                AdbUtil.screenshot()
+            else:
+                pass
+            Memorydata.append(memorydata)
+            i+=1
+        return Memoryerror,Memorydata
         pass
